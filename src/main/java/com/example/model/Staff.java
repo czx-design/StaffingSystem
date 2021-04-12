330651391c96541be27c8dbb98c0a790c2e0bdb3
czx-design <chenzhix1999@outlook.com>
2021-04-12 15:50:32 +0800

    用户模型

--- src/main/java/com/example/model/Staff.java
+++ src/main/java/com/example/model/Staff.java
@@ -9,11 +9,25 @@ public class Staff {
     private LocalDateTime gmtCreated;
     //离职时间
     private LocalDateTime gmtModified;
-    //
+    //手机号码
     private String telephone;
+    //email
+    private String email;
+    //员工IP
     private String ip;
+    //员工部门
     private String post;
+    //员工职务
     private String department;
+    //员工权限(1-9级)
+    private int limits;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }

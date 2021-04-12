c74eeda1e6e0a3a394e711342bb44c977d85e02d
czx-design <chenzhix1999@outlook.com>
2021-04-12 15:40:49 +0800

    开始项目

---
+++ src/main/java/com/example/model/Staff.java
@@ -0,0 +1,19 @@
+package com.example.model;
+
+import java.time.LocalDateTime;
+
+public class Staff {
+    private String name;
+    private String id;
+    //入职时间
+    private LocalDateTime gmtCreated;
+    //离职时间
+    private LocalDateTime gmtModified;
+    //
+    private String telephone;
+    private String ip;
+    private String post;
+    private String department;
+
+
+}
